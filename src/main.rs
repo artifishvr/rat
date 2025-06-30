b9d6d2a728bb9e58ef02eee3517c7508c4dbaa54
Arti <59352535+artifishvr@users.noreply.github.com>
2025-06-29 19:03:45 -0700

    rat v2 - 6.7mb to 0.4mb

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,97 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
-#![allow(rustdoc::missing_crate_level_docs)] // it's an example
 
-use eframe::egui;
-use egui::Color32;
-use egui::Style;
-use egui::Theme;
+use image::ImageReader;
+use std::io::Cursor;
+use std::num::NonZeroU32;
+use std::rc::Rc;
+use winit::event::{Event, WindowEvent};
+use winit::event_loop::{ControlFlow, EventLoop};
+use winit::window::Window;
+use winit::window::WindowButtons;
 
-fn main() -> eframe::Result {
-    env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
-    let options = eframe::NativeOptions {
-        viewport: egui::ViewportBuilder::default().with_inner_size([750.0, 450.0]),
-        ..Default::default()
-    };
-    eframe::run_native(
-        "rat",
-        options,
-        Box::new(|cc| {
-            setup_custom_style(&cc.egui_ctx);
-            egui_extras::install_image_loaders(&cc.egui_ctx);
-            Ok(Box::<MyApp>::default())
-        }),
+#[path = "./winit_app.rs"]
+mod winit_app;
+
+fn main() {
+    let image_bytes = include_bytes!("./rat.webp");
+
+    let img = ImageReader::new(Cursor::new(image_bytes))
+        .with_guessed_format()
+        .expect("Failed to guess image format")
+        .decode()
+        .expect("Failed to decode image")
+        .to_rgba8();
+
+    let (img_width, img_height) = img.dimensions();
+
+    let event_loop = EventLoop::new().unwrap();
+
+    let mut app = winit_app::WinitAppBuilder::with_init(
+        |elwt| {
+            let window_attributes = Window::default_attributes()
+                .with_title("rat (not a virus)")
+                .with_inner_size(winit::dpi::LogicalSize::new(img_width, img_height))
+                .with_resizable(false)
+                .with_enabled_buttons(WindowButtons::CLOSE);
+
+            let window = {
+                let window = elwt.create_window(window_attributes);
+                Rc::new(window.unwrap())
+            };
+            let context = softbuffer::Context::new(window.clone()).unwrap();
+
+            (window, context)
+        },
+        |_elwt, (window, context)| softbuffer::Surface::new(context, window.clone()).unwrap(),
     )
-}
+    .with_event_handler(|(window, _context), surface, event, elwt| {
+        elwt.set_control_flow(ControlFlow::Wait);
 
-#[derive(Default)]
-struct MyApp {}
+        match event {
+            Event::WindowEvent {
+                window_id,
+                event: WindowEvent::RedrawRequested,
+            } if window_id == window.id() => {
+                let Some(surface) = surface else {
+                    eprintln!("RedrawRequested fired before Resumed or after Suspended");
+                    return;
+                };
+                let (width, height) = {
+                    let size = window.inner_size();
+                    (size.width, size.height)
+                };
+                surface
+                    .resize(
+                        NonZeroU32::new(width).unwrap(),
+                        NonZeroU32::new(height).unwrap(),
+                    )
+                    .unwrap();
 
-impl eframe::App for MyApp {
-    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        egui::CentralPanel::default().show(ctx, |ui| {
-            ui.image(egui::include_image!("rat.png"));
-        });
-    }
-}
+                let mut buffer = surface.buffer_mut().unwrap();
 
-fn setup_custom_style(ctx: &egui::Context) {
-    ctx.style_mut_of(Theme::Light, white_bg);
-    ctx.style_mut_of(Theme::Dark, white_bg);
-}
+                buffer.fill(0);
 
-fn white_bg(style: &mut Style) {
-    style.visuals.panel_fill = Color32::WHITE;
+                for y in 0..img_height.min(height) {
+                    for x in 0..img_width.min(width) {
+                        let pixel = img.get_pixel(x, y);
+                        let [r, g, b, _a] = pixel.0;
+                        let color = (r as u32) << 16 | (g as u32) << 8 | (b as u32);
+                        let buffer_index = (y * width + x) as usize;
+                        buffer[buffer_index] = color;
+                    }
+                }
+
+                buffer.present().unwrap();
+            }
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                window_id,
+            } if window_id == window.id() => {
+                elwt.exit();
+            }
+            _ => {}
+        }
+    });
+
+    event_loop.run_app(&mut app).unwrap();
 }
